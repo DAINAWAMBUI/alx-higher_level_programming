c04d6844f7c0b2fbff761368b844fae487639da2
DAINAWAMBUI <daina.muiruri@gmail.com>
2023-03-07 09:13:52 -0800

    Write the Python function def magic_calculation(a, b, c)

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
-# 101-remove_char_at.py
-
-
-def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
-
-
-=========================
-
-102-magic_calculation.py
-
-#!/usr/bin/python3
 # 102-magic_calculation.py
 
 
@@ -24,3 +9,4 @@ def magic_calculation(a, b, c):
     if c > b:
         return (a + b)
     return (a*b - c)
+
